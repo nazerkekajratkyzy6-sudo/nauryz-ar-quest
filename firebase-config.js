d742b345e4daf80b29f2db38ab43fca38d8aa630
nazerkekajratkyzy6-sudo <nazerkekajratkyzy6@gmail.com>
2026-03-17 10:05:49 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,35 @@
-<script type="module">
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, doc, setDoc, updateDoc, onSnapshot, collection, query, orderBy } 
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  update,
+  get,
+  onValue,
+  query,
+  orderByChild
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "XXX",
-  authDomain: "XXX",
-  projectId: "XXX",
+  apiKey: "AIzaSyDjITxqMGbT7fb9R-rl2rFHZcQvCoWgsgc",
+  authDomain: "nauryz-ar-quest.firebaseapp.com",
+  databaseURL: "https://nauryz-ar-quest-default-rtdb.firebaseio.com",
+  projectId: "nauryz-ar-quest",
+  storageBucket: "nauryz-ar-quest.firebasestorage.app",
+  messagingSenderId: "985856346374",
+  appId: "1:985856346374:web:80b757a445f83785d93687"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getDatabase(app);
 
 window.FB = {
-db, doc, setDoc, updateDoc, onSnapshot, collection, query, orderBy
+  db,
+  ref,
+  set,
+  update,
+  get,
+  onValue,
+  query,
+  orderByChild
 };
-
-</script>
